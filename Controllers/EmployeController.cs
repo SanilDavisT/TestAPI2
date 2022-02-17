5ecfe6fdf445d7e48c92f039adfb73ae96cef06c
Sanil Davis <sanil.davis@speehive.com>
2022-02-17 23:35:44 +0530

    Add project files.

---
+++ Controllers/EmployeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeController : ControllerBase
+    {
+        [HttpPost]
+        [Route ("AddEmploye")] 
+        public Employe AddEmployee(Employe employe)
+        {
+            return employe;
+            
+        }
+
+        [HttpGet]
+        [Route ("GetEmploye")]
+
+        public List<Employe> GetEmploye()
+        {
+            var Employeelist = new List<Employe>();
+
+           Employe e1 = new Employe();
+            e1.name = "Sanil";
+            e1.place = "Thrissur";
+
+            Employe e2 = new Employe();
+            e2.name = "Ashiq";
+            e2.place = "Palakad";
+
+            return Employeelist;
+             
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Model/Employe.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAPI2.Controllers
+{
+    public class Employe
+    {
+
+
+        public int ID { get; set; }
+
+        public string name { get; set; }
+
+        public string place  { get; set; }
+
+    }
+}
